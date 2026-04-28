1b25fce6fc0d9377459ed3855e4489fea5ede780
djumanov <djumanovdev@gmail.com>
2026-04-28 18:16:11 +0500

    request and response

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,7 +5,7 @@ from .views import register_view, login_view, logout_view
 app_name = 'users'
 
 urlpatterns = [
-    path('register/', register_view, name='register_page'),
+    path('register/<int:pk>', register_view, name='register_page'),
     path('login/', login_view, name='login_page'),
     path('logout/', logout_view, name='logout_page'),
     path('foasdjkfoas/', logout_view, name='contact_page'),
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,8 +1,29 @@
+import json
+
 from django.http import HttpRequest, HttpResponse
 
 
-def register_view(reqeust: HttpRequest) -> HttpResponse:
-    return HttpResponse("Register page")
+def register_view(reqeust: HttpRequest, pk: int) -> HttpResponse:
+    params = reqeust.GET
+
+    min_price = params.get('min_price')
+    max_price = params.get('max_price')
+
+    # data = json.loads(reqeust.body)
+    # print(data)
+
+    headers = reqeust.headers
+    print(headers.get('Content-Type'))
+
+    form_data = reqeust.POST
+    print(form_data.get('email'))
+
+    files = reqeust.FILES
+
+    image = files.get('image')
+    print(image.read())
+
+    return HttpResponse(content=f"Register page: {pk}, {min_price}:{max_price}", status=400)
     
 
 def login_view(reqeust: HttpRequest) -> HttpResponse:
